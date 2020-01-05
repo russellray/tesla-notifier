ee8c1784e1777f826aa756d49f4919e27c3718f0
unknown <russellray16@gmail.com>
2020-01-05 14:58:30 -0600

    initial commit. created check_vehicle.py and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# tesla-notifier
+### check_vehicle.py
+
+This script will check your Tesla vehicle and notify you when it is unplugged during off-peak hours.
+
+ - Pulls Tesla stats via Teslafi.com
+ - Sends SMS notifications using Twilio
+
+#### How to use check_vehicle.py
+1. Set your desired alert preferences at the top of the script
+2. Set environment variables with your Twilio and Teslafi information
+ `teslafi_api`: Teslafi API URL
+ `twilio_sid`: Twilio account SID
+ `twilio_token`: Twilio authentication token
+ `twilio_sms_to`: Number to send notifications to (must be validated in Twilio)
+ `sms_from`: Twilio phone number
---
+++ check_vehicle.py
@@ -0,0 +1,63 @@
+from twilio.rest import Client
+from datetime import datetime
+import time
+import requests
+import os
+
+# getting api, auth, and phone info
+url = os.environ['teslafi_api']
+twilio_sid = os.environ['twilio_sid']
+twilio_token = os.environ['twilio_token']
+sms_to = os.environ['twilio_sms_to']
+sms_from = os.environ['twilio_sms_from']
+
+# setting the alert parameters
+alert_after = "21:00:00"
+alert_before = "23:00:00"
+alert_battery = 50
+
+# setting other variables
+loop = 0
+
+def check_tesla():
+    # sending get request and saving the response as response object 
+    r = requests.get(url = url)
+
+    # extracting car data
+    data = r.json()
+    battery_percent = int(data['battery_level'])
+    car_state = data['carState']
+    car_name = data['vehicle_name']
+
+    return [battery_percent, car_state, car_name]
+
+def send_alert(battery_percent, car_name):
+    # creating twilio client object
+    client = Client(twilio_sid, twilio_token)
+    # converting battery percent to string in order to concatenate
+    battery_percent = str(battery_percent)
+
+    # sending the alert
+    message = client.messages.create(
+        to=sms_to, 
+        from_=sms_from,
+        body=car_name+" is not plugged in! Current battery level: "+battery_percent
+    )
+
+while loop == 0:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    # checking if current time is within the specified alert window
+    if current_time > alert_after and current_time < alert_before:
+        battery_percent, car_state, car_name = check_tesla()
+        if car_state != 'Charging'and battery_percent < alert_battery:
+            # sending alert
+            send_alert(battery_percent, car_name)
+            # sleeping for 60 minutes
+            time.sleep(3600)
+        else:
+            # sleeping for 30 minutes
+            time.sleep(1800)
+    else:
+        # sleeping for 5 minutes
+        time.sleep(300)
